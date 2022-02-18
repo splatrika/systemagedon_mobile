eb0707dbc7271fc430248fb64bde136139e8f56c
daniilbelov <tseflutty@yandex.ru>
2022-02-18 17:03:11 +0300

    Add dash controller

---
+++ Assets/App/Movement/Scripts/DashController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Systemagedon.App.Movement
+{
+    public class DashController : MonoBehaviour
+    {
+        [SerializeField] private WorldTouchController _controller;
+        [SerializeField] private GameObject _providerObject;
+
+
+        private IDashesProvider _provider;
+        private IEnumerable<Dash> _controlledDashes;
+
+
+        private void Start()
+        {
+            OnDashesListUpdated();
+        }
+
+
+        private void OnValidate()
+        {
+            _provider = _providerObject.GetComponent<IDashesProvider>();
+            if (_provider == null)
+            {
+                Debug.LogError("Provider must have component that implements IDashesProvider");
+                _providerObject = null;
+            }
+        }
+
+
+        private void OnEnable()
+        {
+            OnValidate();
+            _provider.DashesListUpdated += OnDashesListUpdated;
+            _controller.Touched += OnTouched;
+        }
+
+
+        private void OnDisable()
+        {
+            _provider.DashesListUpdated -= OnDashesListUpdated;
+            _controller.Touched -= OnTouched;
+        }
+
+
+        private void OnDashesListUpdated()
+        {
+            _controlledDashes = _provider.GetDashes();
+        }
+
+
+        private void OnTouched(GameObject touched)
+        {
+            if (!enabled)
+            {
+                return;
+            }
+            Dash touchedDash = touched.GetComponent<Dash>();
+            bool dashCanControlled = touchedDash
+                && _controlledDashes.FirstOrDefault(dash => dash == touchedDash);
+            if (dashCanControlled)
+            {
+                touchedDash.ApplyDash();
+            }
+        }
+    }
+}
