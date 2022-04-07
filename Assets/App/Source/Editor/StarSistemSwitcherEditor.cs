c850834041b3ba4357fbb09b514a2db5c515cdbe
daniilbelov <tseflutty@yandex.ru>
2022-04-07 23:07:56 +0300

    Add Web gameplay scene to build and optimise resources for web

--- Assets/App/Source/Editor/StarSistemSwitcherEditor.cs
+++ Assets/App/Source/Editor/StarSistemSwitcherEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 using Systemagedon.App.Gameplay;
@@ -36,3 +37,4 @@ public class StarSystemSwitcherEditor : Editor
         _target = target as StarSystemSwitcher;
     }
 }
+#endif
