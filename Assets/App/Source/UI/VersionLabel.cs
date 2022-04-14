f9443e9ef770b2d00205cc840bb9165a4e090f8e
daniilbelov <tseflutty@yandex.ru>
2022-04-14 21:26:58 +0300

    Add version data and update balance

---
+++ Assets/App/Source/UI/VersionLabel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Systemagedon.App
+{
+
+    public class VersionLabel : MonoBehaviour
+    {
+        [SerializeField] private Text _label;
+
+
+        private void Start()
+        {
+            _label.text = $"{VersionData.Major}.{VersionData.Minor}." +
+                $"{VersionData.Patch}";
+        }
+    }
+
+}
---
+++ Assets/App/Source/VersionData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Systemagedon.App
+{
+
+    public static class VersionData
+    {
+        public const int Major = 0;
+        public const int Minor = 2;
+        public const int Patch = 5;
+    }
+
+}
